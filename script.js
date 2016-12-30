42c72cc2142b02cab991c12e9cddb9fc4538163b
Piotr Hebrowski <piotr.hebrowski@wp.pl>
2016-12-30 21:30:00 +0100

    js file update

--- script.js
+++ script.js
@@ -10,6 +10,17 @@ function getTriangleArea(a, h){
 console.log( getTriangleArea(10,6) );	
 
 var triangleArea1 = getTriangleArea(2, 3);
+
+console.log(triangleArea1);
+
 var triangleArea2 = getTriangleArea(4, 5);
+
+console.log(triangleArea2);
+
 var triangleArea3 = getTriangleArea(6, 7);
-var triangleArea4 = getTriangleArea(0, 7);
+
+console.log(triangleArea3);
+
+var triangleArea4 = getTriangleArea(-2, 7);
+
+console.log(triangleArea4);
