41863e25a9104cefec65e7051e5b1041ba5817a9
Piotr Hebrowski <piotr.hebrowski@wp.pl>
2017-01-05 18:01:16 +0100

    variable typo fix

--- 8_8.js
+++ 8_8.js
@@ -2,7 +2,7 @@
 
   var  text = "Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach",
        animal = "Zielone słonie",
-       animalUp = text2.toUpperCase(),	
+       animalUp = animal.toUpperCase(),	
        newText = text.replace("Papugi", animalUp), 
        newTexthalf = newText.slice( 0, ((newText.length/2) + 1) );
 
